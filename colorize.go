09bee8af10aa02847d7629d845f20c6677a5ca94
Aaron Torres <tcboox@gmail.com>
2013-12-31 10:01:51 -0800

    Go FMT

--- colorize.go
+++ colorize.go
@@ -4,101 +4,100 @@ import "strings"
 
 //A list of the basic unix shell colors
 type Color string
+
 const (
-    COLOR_NONE Color = ""
-    COLOR_RESET Color = "\033[0m"
-    COLOR_BOLD Color = "\033[1m"
-    COLOR_BLACK Color = "\033[0;30m"
-    COLOR_RED Color = "\033[0;31m"
-    COLOR_GREEN Color = "\033[0;32m"
-    COLOR_YELLOW Color = "\033[0;33m"
-    COLOR_BLUE Color = "\033[0;34m"
-    COLOR_MAGENTA Color = "\033[0;35m"
-    COLOR_CYAN Color = "\033[0;36m"
+	COLOR_NONE    Color = ""
+	COLOR_RESET   Color = "\033[0m"
+	COLOR_BOLD    Color = "\033[1m"
+	COLOR_BLACK   Color = "\033[0;30m"
+	COLOR_RED     Color = "\033[0;31m"
+	COLOR_GREEN   Color = "\033[0;32m"
+	COLOR_YELLOW  Color = "\033[0;33m"
+	COLOR_BLUE    Color = "\033[0;34m"
+	COLOR_MAGENTA Color = "\033[0;35m"
+	COLOR_CYAN    Color = "\033[0;36m"
 
-    COLOR_LIGHT_GRAY Color = "\033[0;37m"
-    COLOR_GRAY Color = "\033[1;30m"
+	COLOR_LIGHT_GRAY Color = "\033[0;37m"
+	COLOR_GRAY       Color = "\033[1;30m"
 
-    COLOR_BOLD_RED Color = "\033[1;31m"
-    COLOR_BOLD_GREEN Color = "\033[1;32m"
-    COLOR_BOLD_YELLOW Color = "\033[1;33m"
-    COLOR_BOLD_BLUE Color = "\033[1;34m"
-    COLOR_BOLD_MAGENTA Color = "\033[1;35m"
-    COLOR_BOLD_CYAN Color = "\033[1;36m"
-    COLOR_WHITE Color = "\033[1;37m"
+	COLOR_BOLD_RED     Color = "\033[1;31m"
+	COLOR_BOLD_GREEN   Color = "\033[1;32m"
+	COLOR_BOLD_YELLOW  Color = "\033[1;33m"
+	COLOR_BOLD_BLUE    Color = "\033[1;34m"
+	COLOR_BOLD_MAGENTA Color = "\033[1;35m"
+	COLOR_BOLD_CYAN    Color = "\033[1;36m"
+	COLOR_WHITE        Color = "\033[1;37m"
 )
 
 type BgColor string
-const (
-    COLOR_BG_NONE BgColor = ""
-    COLOR_BG_RED BgColor = "\033[41m"
-    COLOR_BG_GREEN BgColor = "\033[42m" 
-    COLOR_BG_YELLOW BgColor = "\033[43m" 
-    COLOR_BG_BLUE BgColor = "\033[44m"
-    COLOR_BG_MAGENTA BgColor = "\033[45m"
-    COLOR_BG_CYAN BgColor = "\033[46m"
-    COLOR_BG_WHITE BgColor = "\033[47m"
 
+const (
+	COLOR_BG_NONE    BgColor = ""
+	COLOR_BG_RED     BgColor = "\033[41m"
+	COLOR_BG_GREEN   BgColor = "\033[42m"
+	COLOR_BG_YELLOW  BgColor = "\033[43m"
+	COLOR_BG_BLUE    BgColor = "\033[44m"
+	COLOR_BG_MAGENTA BgColor = "\033[45m"
+	COLOR_BG_CYAN    BgColor = "\033[46m"
+	COLOR_BG_WHITE   BgColor = "\033[47m"
 )
 
-type bold bool
-
-type Colorize struct{
-    Color
-    BgColor
+type Colorize struct {
+	Color
+	BgColor
 }
 
-func New(c Color, b BgColor) Colorize{
-    var result Colorize
-    result.SetColor(c)
-    result.SetBgColor(b)
-    return result
+func New(c Color, b BgColor) Colorize {
+	var result Colorize
+	result.SetColor(c)
+	result.SetBgColor(b)
+	return result
 }
 
-func (C *Colorize) SetColor(c Color){
-    C.Color = c
+func (C *Colorize) SetColor(c Color) {
+	C.Color = c
 }
 
-func (C *Colorize) SetBgColor(b BgColor){
-    C.BgColor = b
+func (C *Colorize) SetBgColor(b BgColor) {
+	C.BgColor = b
 }
 
-func (C *Colorize) Fmt(val string) string{
-    var s []string
-    s = append(s, string(C.Color))
-    if C.BgColor != COLOR_BG_NONE{
-        s = append(s, string(C.BgColor))
-    }
-    s = append(s, val)
-    s = append(s, string(COLOR_RESET))
+func (C *Colorize) Fmt(val string) string {
+	var s []string
+	s = append(s, string(C.Color))
+	if C.BgColor != COLOR_BG_NONE {
+		s = append(s, string(C.BgColor))
+	}
+	s = append(s, val)
+	s = append(s, string(COLOR_RESET))
 
-    result := strings.Join(s, "")
-    return result
+	result := strings.Join(s, "")
+	return result
 }
 
-func (C *Colorize) FmtColor(val string, c Color) string{
-    var orig_color = C.Color
-    C.Color = c
-    result := C.Fmt(val)
-    C.Color = orig_color
-    return result
+func (C *Colorize) FmtColor(val string, c Color) string {
+	var orig_color = C.Color
+	C.Color = c
+	result := C.Fmt(val)
+	C.Color = orig_color
+	return result
 }
 
-func (C *Colorize) FmtBgColor(val string, b BgColor) string{
-    var orig_bg_color = C.BgColor
-    C.BgColor = b
-    result := C.Fmt(val)
-    C.BgColor = orig_bg_color
-    return result
+func (C *Colorize) FmtBgColor(val string, b BgColor) string {
+	var orig_bg_color = C.BgColor
+	C.BgColor = b
+	result := C.Fmt(val)
+	C.BgColor = orig_bg_color
+	return result
 }
 
-func (C *Colorize) FmtColorAndBgColor(val string, c Color, b BgColor) string{
-    var orig_color = C.Color
-    var orig_bg_color = C.BgColor
-    C.Color = c
-    C.BgColor = b
-    result := C.Fmt(val)
-    C.Color = orig_color
-    C.BgColor = orig_bg_color
-    return result
+func (C *Colorize) FmtColorAndBgColor(val string, c Color, b BgColor) string {
+	var orig_color = C.Color
+	var orig_bg_color = C.BgColor
+	C.Color = c
+	C.BgColor = b
+	result := C.Fmt(val)
+	C.Color = orig_color
+	C.BgColor = orig_bg_color
+	return result
 }
--- colorize_test.go
+++ colorize_test.go
@@ -1,96 +1,95 @@
 package colorize
 
 import (
-    "fmt"
-    "log"
-    "testing"
+	"fmt"
+	"log"
+	"testing"
 )
 
 var (
-    DEBUG *log.Logger   
-    WARN *log.Logger
+	DEBUG *log.Logger
+	WARN  *log.Logger
 )
 
-func TestStatefulColor(t *testing.T){
-    var red, blue Colorize
-    
-    //set some state
-    red.SetColor(COLOR_RED)
-    blue.SetColor(COLOR_BLUE)
-    
-    out_string := fmt.Sprintf("This %s a %s", red.Fmt("is"), blue.Fmt("test"))
-    basis_string := "This \033[0;31mis\033[0m a \033[0;34mtest\033[0m"
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+func TestStatefulColor(t *testing.T) {
+	var red, blue Colorize
+
+	//set some state
+	red.SetColor(COLOR_RED)
+	blue.SetColor(COLOR_BLUE)
+
+	out_string := fmt.Sprintf("This %s a %s", red.Fmt("is"), blue.Fmt("test"))
+	basis_string := "This \033[0;31mis\033[0m a \033[0;34mtest\033[0m"
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
 
-func TestStatefulBGColor(t *testing.T){
-    var white_red_bg Colorize
+func TestStatefulBGColor(t *testing.T) {
+	var white_red_bg Colorize
 
-    //set color and background
-    white_red_bg.SetColor(COLOR_WHITE)
-    white_red_bg.SetBgColor(COLOR_BG_RED)
-    
-    out_string := fmt.Sprintf(white_red_bg.Fmt("Now with backgrounds!"))
-    basis_string := "\033[1;37m\033[41mNow with backgrounds!\033[0m"
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+	//set color and background
+	white_red_bg.SetColor(COLOR_WHITE)
+	white_red_bg.SetBgColor(COLOR_BG_RED)
+
+	out_string := fmt.Sprintf(white_red_bg.Fmt("Now with backgrounds!"))
+	basis_string := "\033[1;37m\033[41mNow with backgrounds!\033[0m"
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
 
-func TestStatelessColor(t *testing.T){
-    var generic Colorize
+func TestStatelessColor(t *testing.T) {
+	var generic Colorize
 
-    out_string := fmt.Sprintf(generic.FmtColor("on the fly", COLOR_GREEN)) 
-    basis_string := "\033[0;32mon the fly\033[0m"
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+	out_string := fmt.Sprintf(generic.FmtColor("on the fly", COLOR_GREEN))
+	basis_string := "\033[0;32mon the fly\033[0m"
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
 
-func TestStatelessBackground(t *testing.T){
-    var generic Colorize
+func TestStatelessBackground(t *testing.T) {
+	var generic Colorize
 
-    out_string := fmt.Sprintf(generic.FmtBgColor("Yellow Background", COLOR_BG_YELLOW)) 
-    basis_string := "\033[43mYellow Background\033[0m"
+	out_string := fmt.Sprintf(generic.FmtBgColor("Yellow Background", COLOR_BG_YELLOW))
+	basis_string := "\033[43mYellow Background\033[0m"
 
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
 
-func TestStatelessColorAndBackground(t *testing.T){
-    var generic Colorize
+func TestStatelessColorAndBackground(t *testing.T) {
+	var generic Colorize
 
-    out_string := fmt.Sprintf(generic.FmtColorAndBgColor("Cyan on Magenta??", COLOR_CYAN, COLOR_BG_MAGENTA)) 
-    basis_string := "\033[0;36m\033[45mCyan on Magenta??\033[0m"
+	out_string := fmt.Sprintf(generic.FmtColorAndBgColor("Cyan on Magenta??", COLOR_CYAN, COLOR_BG_MAGENTA))
+	basis_string := "\033[0;36m\033[45mCyan on Magenta??\033[0m"
 
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
 
-func TestNew(t *testing.T){
-    black_on_white := New(COLOR_BLACK, COLOR_BG_WHITE)
-    f := black_on_white.Fmt
+func TestNew(t *testing.T) {
+	black_on_white := New(COLOR_BLACK, COLOR_BG_WHITE)
+	f := black_on_white.Fmt
 
-    out_string := fmt.Sprintf(f("Now this is cool"))
-    basis_string := "\033[0;30m\033[47mNow this is cool\033[0m"
-    if out_string != basis_string{
-        t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
-    }else{
-        fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
-    }
+	out_string := fmt.Sprintf(f("Now this is cool"))
+	basis_string := "\033[0;30m\033[47mNow this is cool\033[0m"
+	if out_string != basis_string {
+		t.Errorf("Error: string '%s' does not match '%s'\n", out_string, basis_string)
+	} else {
+		fmt.Printf("Success: string: '%s' matches '%s'\n", out_string, basis_string)
+	}
 }
-
